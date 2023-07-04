63ec39924a6e856949b2f83dce5b0dcc2185932c
Marius Toporascu <mariustoporascu@topodvlp.ro>
2023-07-04 20:30:21 +0300

    fav-users WIP

--- LW.BkEndLogic/Commons/DbRepoCommon.cs
+++ LW.BkEndLogic/Commons/DbRepoCommon.cs
@@ -5,83 +5,154 @@ using Microsoft.EntityFrameworkCore;
 
 namespace LW.BkEndLogic.Commons
 {
-	public class DbRepoCommon : IDbRepoCommon
-	{
-		private readonly LwDBContext _context;
-		public DbRepoCommon(LwDBContext context)
-		{
-			_context = context;
-		}
-		public async Task<bool> EmailNotTaken(string email)
-		{
-			return !(await _context.Users.AnyAsync(usr => usr.Email == email));
-		}
-		public IEnumerable<object> FindUsers(string emailOrPhone)
-		{
-			var users = _context.Users
-				.Where(usr => (usr.Email.Contains(emailOrPhone) || usr.PhoneNumber.Contains(emailOrPhone)) &&
-				 usr.ConexiuniConturi.FirmaDiscountId == null && usr.ConexiuniConturi.HybridId == null);
-			var usersList = new List<object>();
-			foreach
-				(var usr in users)
-			{
-				var conex = _context.ConexiuniConturi.Include(c => c.ProfilCont).FirstOrDefault(con => con.UserId == usr.Id);
-				if (conex == null)
-				{
-					continue;
-				}
-				usersList.Add(new
-				{
-					fullName = $"{conex.ProfilCont?.Name} {conex.ProfilCont?.FirstName}",
-					phoneNumber = usr.PhoneNumber,
-					conexId = _context.ConexiuniConturi.FirstOrDefault(con => con.UserId == usr.Id)?.Id,
-				});
-			}
-			return usersList;
-		}
-		public async Task<bool> AddCommonEntity<T>(T entity)
-		{
-			_context.Add(entity);
-			return await SaveChangesAsync();
-		}
-		public async Task<bool> DeleteCommonEntity<T>(T entity)
-		{
-			_context.Remove(entity);
-			return await SaveChangesAsync();
-		}
-		public IEnumerable<FirmaDiscount> GetAllFolders()
-		{
-			return _context.FirmaDiscount.Where(x => x.IsActive)
-				.Select(x => new FirmaDiscount
-				{
-					Id = x.Id,
-					Name = x.Name,
-					CuiNumber = x.CuiNumber,
-					DiscountPercent = x.DiscountPercent,
-					IsActive = x.IsActive,
-				}).AsEnumerable();
-		}
-		public IEnumerable<FirmaDiscount> GetAllFolders(Guid hybridId)
-		{
-			var firmaDiscountId = _context.Hybrid.FirstOrDefault(x => x.Id == hybridId)?.FirmaDiscountId;
-			return _context.FirmaDiscount.Where(x => x.IsActive && x.Id == firmaDiscountId)
-				.Select(x => new FirmaDiscount
-				{
-					Id = x.Id,
-					Name = x.Name,
-					CuiNumber = x.CuiNumber,
-					DiscountPercent = x.DiscountPercent,
-					IsActive = x.IsActive,
-				}).AsEnumerable();
-		}
-		public async Task<bool> UpdateCommonEntity<T>(T entity)
-		{
-			_context.Update(entity);
-			return await SaveChangesAsync();
-		}
-		private async Task<bool> SaveChangesAsync()
-		{
-			return await _context.SaveChangesAsync() > 0;
-		}
-	}
+    public class DbRepoCommon : IDbRepoCommon
+    {
+        private readonly LwDBContext _context;
+
+        public DbRepoCommon(LwDBContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<bool> AddFavoriteUser(Guid conexId, Guid favConexId)
+        {
+            var newFavorite = new PreferinteHybrid { ConexId = conexId, MyConexId = favConexId, };
+            return await AddCommonEntity(newFavorite);
+        }
+
+        public async Task<bool> RemoveFavoriteUser(Guid conexId, Guid favConexId)
+        {
+            var favorite = _context.PreferinteHybrid.FirstOrDefault(
+                fav => fav.ConexId == conexId && fav.MyConexId == favConexId
+            );
+            if (favorite == null)
+            {
+                return false;
+            }
+            return await DeleteCommonEntity(favorite);
+        }
+
+        public IEnumerable<object> GetFavoritesList(Guid conexId)
+        {
+            var favorites = _context.PreferinteHybrid.Where(fav => fav.ConexId == conexId);
+            var favoritesList = new List<object>();
+            foreach (var fav in favorites)
+            {
+                var conex = _context.ConexiuniConturi
+                    .Include(c => c.ProfilCont)
+                    .FirstOrDefault(con => con.Id == fav.MyConexId);
+                favoritesList.Add(
+                    new
+                    {
+                        conexId = conex.Id,
+                        fullName = $"{conex.ProfilCont?.Name} {conex.ProfilCont?.FirstName}",
+                        phoneNumber = conex.ProfilCont.PhoneNumber,
+                        email = conex.ProfilCont.Email,
+                    }
+                );
+            }
+            return favoritesList;
+        }
+
+        public async Task<bool> EmailNotTaken(string email)
+        {
+            return !(await _context.Users.AnyAsync(usr => usr.Email == email));
+        }
+
+        public IEnumerable<object> FindUsers(string emailOrPhone)
+        {
+            var users = _context.Users.Where(
+                usr =>
+                    (
+                        usr.Email.ToLower().Contains(emailOrPhone.ToLower())
+                        || usr.PhoneNumber.Contains(emailOrPhone)
+                    )
+                    && usr.ConexiuniConturi.FirmaDiscountId == null
+                    && usr.ConexiuniConturi.HybridId == null
+            );
+            var usersList = new List<object>();
+            foreach (var usr in users)
+            {
+                var conex = _context.ConexiuniConturi
+                    .Include(c => c.ProfilCont)
+                    .FirstOrDefault(con => con.UserId == usr.Id);
+                if (conex == null)
+                {
+                    continue;
+                }
+                usersList.Add(
+                    new
+                    {
+                        conexId = conex.Id,
+                        fullName = $"{conex.ProfilCont?.Name} {conex.ProfilCont?.FirstName}",
+                        phoneNumber = usr.PhoneNumber,
+                        email = conex.ProfilCont.Email,
+                    }
+                );
+            }
+            return usersList;
+        }
+
+        public async Task<bool> AddCommonEntity<T>(T entity)
+        {
+            _context.Add(entity);
+            return await SaveChangesAsync();
+        }
+
+        public async Task<bool> DeleteCommonEntity<T>(T entity)
+        {
+            _context.Remove(entity);
+            return await SaveChangesAsync();
+        }
+
+        public IEnumerable<FirmaDiscount> GetAllFolders()
+        {
+            return _context.FirmaDiscount
+                .Where(x => x.IsActive)
+                .Select(
+                    x =>
+                        new FirmaDiscount
+                        {
+                            Id = x.Id,
+                            Name = x.Name,
+                            CuiNumber = x.CuiNumber,
+                            DiscountPercent = x.DiscountPercent,
+                            IsActive = x.IsActive,
+                        }
+                )
+                .AsEnumerable();
+        }
+
+        public IEnumerable<FirmaDiscount> GetAllFolders(Guid hybridId)
+        {
+            var firmaDiscountId = _context.Hybrid
+                .FirstOrDefault(x => x.Id == hybridId)
+                ?.FirmaDiscountId;
+            return _context.FirmaDiscount
+                .Where(x => x.IsActive && x.Id == firmaDiscountId)
+                .Select(
+                    x =>
+                        new FirmaDiscount
+                        {
+                            Id = x.Id,
+                            Name = x.Name,
+                            CuiNumber = x.CuiNumber,
+                            DiscountPercent = x.DiscountPercent,
+                            IsActive = x.IsActive,
+                        }
+                )
+                .AsEnumerable();
+        }
+
+        public async Task<bool> UpdateCommonEntity<T>(T entity)
+        {
+            _context.Update(entity);
+            return await SaveChangesAsync();
+        }
+
+        private async Task<bool> SaveChangesAsync()
+        {
+            return await _context.SaveChangesAsync() > 0;
+        }
+    }
 }
--- LW.BkEndLogic/Commons/Interfaces/IDbRepoCommon.cs
+++ LW.BkEndLogic/Commons/Interfaces/IDbRepoCommon.cs
@@ -2,15 +2,17 @@
 
 namespace LW.BkEndLogic.Commons.Interfaces
 {
-	public interface IDbRepoCommon
-	{
-		IEnumerable<FirmaDiscount> GetAllFolders();
-		IEnumerable<FirmaDiscount> GetAllFolders(Guid hybridId);
-		IEnumerable<object> FindUsers(string emailOrPhone);
-		Task<bool> EmailNotTaken(string email);
-		Task<bool> AddCommonEntity<T>(T entity);
-		Task<bool> UpdateCommonEntity<T>(T entity);
-		Task<bool> DeleteCommonEntity<T>(T entity);
-	}
-
+    public interface IDbRepoCommon
+    {
+        IEnumerable<FirmaDiscount> GetAllFolders();
+        IEnumerable<FirmaDiscount> GetAllFolders(Guid hybridId);
+        IEnumerable<object> FindUsers(string emailOrPhone);
+        Task<bool> RemoveFavoriteUser(Guid conexId, Guid favConexId);
+        Task<bool> AddFavoriteUser(Guid conexId, Guid favConexId);
+        IEnumerable<object> GetFavoritesList(Guid conexId);
+        Task<bool> EmailNotTaken(string email);
+        Task<bool> AddCommonEntity<T>(T entity);
+        Task<bool> UpdateCommonEntity<T>(T entity);
+        Task<bool> DeleteCommonEntity<T>(T entity);
+    }
 }
